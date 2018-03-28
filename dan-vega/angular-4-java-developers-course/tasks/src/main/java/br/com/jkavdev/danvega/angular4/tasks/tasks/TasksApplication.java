05bc95625cdfb3f1af25b90e4662205cd98c7bf0
jkavdev <jhonatankolen@gmail.com>
2018-03-28 00:35:29 -0300

    02 - Angular 4 Java Developers - Creating the Spring Boot Application
    - Criando o projeto spring boot

---
+++ dan-vega/angular-4-java-developers-course/tasks/src/main/java/br/com/jkavdev/danvega/angular4/tasks/tasks/TasksApplication.java
@@ -0,0 +1,12 @@
+package br.com.jkavdev.danvega.angular4.tasks.tasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasksApplication.class, args);
+	}
+}
